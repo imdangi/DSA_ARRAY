75e7beb201388b83ce7787fc3e2687b8fe7370d2
Manoj Kumar <82015676+imdangi@users.noreply.github.com>
2021-06-21 17:14:15 +0530

    Added New file

---
+++ 1_creatingArray.py
@@ -0,0 +1,12 @@
+# Here we are not using list predefined function , we will make pure logic of array
+n = int(input("Enter how many elements you want to add in array : "))
+arr = []
+for i in range(n):
+    temp = int(input(f"Enter value  {i+1} of array : "))
+    arr.append(temp) #--> list will overwrite with new values every time , and we will have only last element
+                    #--> this is same as scanf("%d",&arr[i]); in c
+# Printing values of array , Traversal operation
+for i in arr:
+    print(i, end=" ")
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DSA_ARRAY
 # I will add codes only in pure logic , no predefined functions and method i will use
+# File 1 :- 1. Creating Array
+# File 2 :- 2. 
