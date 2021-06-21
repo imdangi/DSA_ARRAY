d826fa35958fa06ab2bebb5cadaabd06a666523a
Manoj Kumar <82015676+imdangi@users.noreply.github.com>
2021-06-21 17:18:21 +0530

    Updated file

--- 1_creatingArray.py
+++ 1_creatingArray.py
@@ -1,5 +1,5 @@
 # Here we are not using list predefined function , we will make pure logic of array
-n = int(input("Enter how many elements you want to add in array : "))
+n =int(input("Enter how many elements you want to add in array : "))
 arr = []
 for i in range(n):
     temp = int(input(f"Enter value  {i+1} of array : "))
@@ -8,7 +8,7 @@ for i in range(n):
 # Printing values of array , Traversal operation
 for i in arr:
     print(i, end=" ")
-print("Printing Again with another way of using for loop in python :- ")
+print("\nPrinting Again with another way of using for loop in python :- ")
 for i in range(n):
     print(arr[i],end=" ")
 
